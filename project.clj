64674a6f4601cfd994b03395d66a4ba8d3f31570
Christopher Miles <twitch@nervestaple.com>
2010-01-18 16:42:35 -0500

    Removed extraneous dependencies.

--- project.clj
+++ project.clj
@@ -2,9 +2,7 @@
   :description "A parser for parsing HL7 messages."
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [commons-logging/commons-logging "1.1.1"]
-                 [commons-net/commons-net "2.0"]
-                 [net.sourceforge.jtds/jtds "1.2.2"]]
+                 [commons-logging/commons-logging "1.1.1"]]
   :dev-dependencies [[org.clojure/swank-clojure "1.0"]]
   :repositories [["cdh" "http://cdhintranet01.cooley-dickinson.org/nexus/content/groups/public"]]
   :main "org.cooleydickinson.hl7_parser.main")
