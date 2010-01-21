6b0eebeb1e501d82ac64568f1691a6cdd0f62331
Christopher Miles <twitch@nervestaple.com>
2010-01-21 15:29:24 -0500

    Cleaned up the project and pom files.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojure-hl7-parser "1.0-SNAPSHOT"
   :description "A parser for parsing HL7 messages."
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
                  [commons-logging/commons-logging "1.1.1"]]
   :dev-dependencies [[org.clojure/swank-clojure "1.0"]]
   :repositories [["cdh" "http://cdhintranet01.cooley-dickinson.org/nexus/content/groups/public"]]
