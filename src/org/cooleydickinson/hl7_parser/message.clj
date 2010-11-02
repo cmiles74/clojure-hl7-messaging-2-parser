96a1c13eb6111dce342f37ff186d66f061b69e15
Christopher Miles <chris_miles@cooley-dickinson.org>
2010-11-02 11:03:21 -0400

    Coded up a method to alter a field in a message.

--- src/org/cooleydickinson/hl7_parser/message.clj
+++ src/org/cooleydickinson/hl7_parser/message.clj
@@ -1,7 +1,12 @@
 ;;
 ;; Functions to make it easier to work with parsed HL7 messages.
 ;;
-(ns org.cooleydickinson.hl7-parser.message)
+(ns org.cooleydickinson.hl7-parser.message
+  (:use
+   [org.cooleydickinson.hl7-parser.parser2]
+   [org.cooleydickinson.hl7-parser.util]
+   [org.cooleydickinson.hl7-parser.dump]
+   [org.cooleydickinson.hl7-parser.message]))
 
 (defn segment-ids
   "Returns a list of the segment ids present in the message."
@@ -69,27 +74,71 @@
   (map (fn [segment] (get-segment-field segment field-index))
        (get-segments message segment-id)))
 
+(defn- get-nth-field
+  "Returns the item at index in the collection of field data. If
+  passed a collection of fields, the item at index from each field is
+  returned. If passed a collection that contains subcomponents, the
+  item at index for each subcomponent is returned."
+  [index field-or-fields]
+  (cond
+    (map? (first field-or-fields))
+    (map (fn [field]
+           (get-nth-field index (:content field)))
+         field-or-fields)
+
+    (coll? (first field-or-fields))
+    (map (partial get-nth-field index) field-or-fields)
+
+    :else
+    (nth field-or-fields index)))
+
 (defn get-field-component
   "Returns the component at the provided index from the field with the
 provided index from the segment with the given id in the provided
 message."
   [message segment-id field-index component-index]
-  (apply (fn [results]
-           (if (list? (first results))
-             (first results)
-             results))
-         
-         (map (fn [field-data]
+  (let [data (flatten (get-field message segment-id field-index))]
+    (get-nth-field component-index data)))
 
-                ;; handle repeating fields
-                (if (map? (first field-data))
-                  (for [field-in field-data]
-                    (nth (:content field-in) component-index))
+(defn set-field
+  "Updates the message by altering the field value for the specified
+  segment. When specifying field indexes, be sure to use the correct
+  HL7 index (the segment id would be 0, the first field is at index
+  1).
 
-                  ;; return the component
-                  (nth field-data component-index)))
+  Your value should be an atom or an collection, a collection
+  indicates a field with components. Subcomponents are represented as
+  a collection containing a collection. Pass in a collection of fields
+  to indicate repeating fields."
+  [message segment-id field-index value]
 
-              ;; select matching segments
-              (map (fn [segment]
-                     (get-segment-field segment field-index))
-                   (get-segments message segment-id)))))
+  ;; correct our index and value (put an atom in a collection)
+  (let [field-index-fixed (dec field-index)
+        field-value (if (coll? value) value [value])]
+
+    ;; throw an error if we have an illegal HL7 index
+    (if (< field-index-fixed 0)
+      (throw (Exception. "The first field is at index 1")))
+
+    ;; create a whole new message
+    (struct-map message-struct
+      :delimiters (:delimiters message)
+
+      ;; map over our segments looking for the one we're changing
+      :segments (map (fn [segment]
+                       
+                       (if (= segment-id (:id segment))
+
+                         ;; associate our new fields
+                         (assoc segment :field
+
+                                ;; associate our new value with the
+                                ;; field collections
+                                (assoc (:fields segment)
+                                  field-index-fixed
+                                  (create-field field-value)))
+
+                         ;; return the segment unaltered
+                         segment))
+
+                     (:segments message)))))
