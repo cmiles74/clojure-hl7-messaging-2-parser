18e519a89a22215e36e82a2cefa572b3331f88bd
Christopher Miles <chris_miles@cooley-dickinson.org>
2010-11-08 09:10:21 -0500

    Incremented the version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-hl7-parser "2.2"
+(defproject clojure-hl7-parser "2.3"
   :description "A parser for parsing HL7 messages."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
