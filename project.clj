67eb924585c35375f4e3318ec25c41acfa48abc6
Christopher Miles <twitch@nervestaple.com>
2021-04-10 11:39:17 -0400

    Changed artifact group

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject clojure-hl7-parser "3.4.2"
+(defproject org.clojars.cmiles74/clojure-hl7-parser "3.4.2"
   :description "A parser for parsing HL7 messages."
+  :url "https://github.com/cmiles74/clojure-hl7-messaging-2-parser"
   :dependencies [[com.taoensso/timbre "5.1.0"]]
   :main com.nervestaple.hl7-parser.main)
