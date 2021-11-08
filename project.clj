85e6f50be319ebba6cde52a4e23e69a65a85e9a7
Christopher Miles <twitch@nervestaple.com>
2021-11-08 13:21:41 -0500

    Incremented version number for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.cmiles74/clojure-hl7-parser "3.4.5-SNAPSHOT"
+(defproject org.clojars.cmiles74/clojure-hl7-parser "3.4.5"
   :description "A parser for parsing HL7 messages."
   :url "https://github.com/cmiles74/clojure-hl7-messaging-2-parser"
   :dependencies [[com.taoensso/timbre "5.1.2"]]
