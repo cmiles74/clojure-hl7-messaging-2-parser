60d735444ca67f9a1ad388076f5596a4ce132cd2
Eero Helenius <eero.helenius@solita.fi>
2023-02-17 10:14:45 +0200

    Drop Timbre dependency

--- src/com/nervestaple/hl7_parser/main.clj
+++ src/com/nervestaple/hl7_parser/main.clj
@@ -6,8 +6,6 @@
 (ns com.nervestaple.hl7-parser.main
   (:gen-class)
   (:require
-   [taoensso.timbre :as timbre
-    :refer (trace debug info warn error fatal spy)]
    [com.nervestaple.hl7-parser.parser :as parser]
    [com.nervestaple.hl7-parser.message :as message]
    [com.nervestaple.hl7-parser.batch :as batch]
@@ -24,18 +22,18 @@
         parsed-message (parser/parse message)]
 
     ;; provide a brief demonstration
-    (info (str "Message Id: "
-               (message/get-field-first-value parsed-message "MSH" 10)))
-    (info (str "MSH Segment: "
-               (pr-str (first (message/get-segments parsed-message "MSH")))))
-    (info (str "ACK: "
-               (message/ack-message {:sending-app "Clojure HL7 Parser"
-                                     :sending-facility "Test Facility"
-                                     :production-mode "P"
-                                     :version "2.3"
-                                     :text-message "Message processed successfully"}
-                                    "AA" parsed-message))
-          (info (str "Parsed: " (pr-str parsed-message))))
+    (println (str "Message Id: "
+                  (message/get-field-first-value parsed-message "MSH" 10)))
+    (println (str "MSH Segment: "
+                  (pr-str (first (message/get-segments parsed-message "MSH")))))
+    (println (str "ACK: "
+                  (message/ack-message {:sending-app "Clojure HL7 Parser"
+                                        :sending-facility "Test Facility"
+                                        :production-mode "P"
+                                        :version "2.3"
+                                        :text-message "Message processed successfully"}
+                                       "AA" parsed-message))
+          (println (str "Parsed: " (pr-str parsed-message))))
 
     ;; (with-open [reader (io/reader "file-of-batch-message.hl7.txt")]
     ;;   (let [messages (batch/read-messages reader)]
