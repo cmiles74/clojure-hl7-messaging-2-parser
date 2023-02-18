8d5f709579886dc6c59c169ed771e9958067a5c0
Christopher Miles <twitch@nervestaple.com>
2023-02-18 13:45:41 -0500

    :heavy_minus_sign: Removes out-of-date dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject org.clojars.cmiles74/clojure-hl7-parser "3.4.5"
   :description "A parser for parsing HL7 messages."
   :url "https://github.com/cmiles74/clojure-hl7-messaging-2-parser"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
   :main com.nervestaple.hl7-parser.main
   :repositories [["clojars" {:url "https://repo.clojars.org" :cred :gpg}]]
   :profiles {:dev {:dependencies [[lambdaisland/kaocha "1.77.1236"]]}}
