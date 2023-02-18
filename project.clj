b3afedcfb8b1772176788cc77fbc8db781083174
Christopher Miles <twitch@nervestaple.com>
2023-02-18 14:12:22 -0500

    :bookmark: Increments version for release
    Nobody runs this JAR, removed references to main namespace from the
    project.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject org.clojars.cmiles74/clojure-hl7-parser "3.4.5"
+(defproject org.clojars.cmiles74/clojure-hl7-parser "3.5.0"
   :description "A parser for parsing HL7 messages."
   :url "https://github.com/cmiles74/clojure-hl7-messaging-2-parser"
-  :main com.nervestaple.hl7-parser.main
   :repositories [["clojars" {:url "https://repo.clojars.org" :cred :gpg}]]
   :profiles {:dev {:dependencies [[lambdaisland/kaocha "1.77.1236"]]}}
   :aliases {"kaocha" ["run" "-m" "kaocha.runner"]})
